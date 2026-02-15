bc0db13e44a1fcf5edf0b478c735138ebfbc2210
MarcoBasileDev <marco.basile1996@gmail.com>
2026-02-15 19:15:48 +0100

    add 1365

---
+++ Solutions/easy/1365_how_many_numbers_are_smaller_than_the_current_number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        freq = [0] * 101
+        res = [0] * len(nums)
+
+        for num in nums:
+            freq[num] += 1
+
+        for num in range(1, 101):
+            freq[num] += freq[num - 1]
+
+        for i in range(len(nums)):
+            v = nums[i]
+
+            if v == 0:
+                res[i] = 0
+            else:
+                res[i] = freq[v - 1]
+
+        return res
