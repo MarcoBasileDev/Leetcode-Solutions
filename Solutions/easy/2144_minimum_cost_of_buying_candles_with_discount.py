fe670ff7dcf5c9fdcb6ed596de06711fea410caa
ItsStillBad <133338735+MarcoBasileDev@users.noreply.github.com>
2026-06-01 09:59:22 +0200

    add lc 2144

---
+++ Solutions/easy/2144_minimum_cost_of_buying_candles_with_discount.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        cost.sort(reverse = True)
+        res = 0
+        n = len(cost)
+
+        for i in range(n):
+            if i % 3 != 2:
+                res += cost[i]
+
+        return res
+
+        # Time Complexity O(n log n)
+        # Space Complexity O(log n)
